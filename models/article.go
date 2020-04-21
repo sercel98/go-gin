f2ca17340bfa92edc25a21bb6b43bddcc84b58b6
sercel98 <sergiocelemin1998@gmail.com>
2020-04-20 23:51:26 -0500

    Connection succesfully to the db Create db.go Create a Configuration struct

--- models/article.go
+++ models/article.go
@@ -1,11 +1,15 @@
 package models
 
-import "time"
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
 
 //Struct Article represents an Article models with his attributes
 type Article struct {
-	Title         string
-	Author        string
-	PublishedDate time.Time
-	Content       string
+	gorm.Model
+	Title         string    `json:"title" gorm:"size:100; not null"`
+	Author        string    `json:"author" gorm:"size:70; not null"`
+	PublishedDate time.Time `json:"publishedDate" gorm:"type:time; not null"`
+	Content       string    `json:"content" gorm:"size:600; not null"`
 }
